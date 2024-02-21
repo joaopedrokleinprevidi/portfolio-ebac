c70939ef6b3977f6333d5f8763032c0c49403dca
* <*>
2024-02-21 09:09:16 -0300

    att

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from "../../components/Titulo"
 import Paragrafo from "../../components/Paragrafo"
+import { GithubSecao } from "./styles"
 
 const sobre = () => {
   return (
@@ -11,6 +12,10 @@ const sobre = () => {
         consequatur, quod repudiandae maiores magni? Accusamus, enim voluptates
         ab quae pariatur eius.
       </Paragrafo>
+      <GithubSecao>
+        <img src="https://github-readme-stats.vercel.app/api?username=joaopedrokleinprevidi&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=joaopedrokleinprevidi&layout=compact&langs_count=7&theme=dracula" />
+      </GithubSecao>
     </section>
   )
 }
